ccdbcef598a3e5e7f416c82aff8e593756992f9a
TimaR0 <trodoslavov@gmail.com>
2023-05-25 17:51:37 +0300

    Add project files.

---
+++ ASP-111/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using ASP_111.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using ASP_111.Services;
+
+namespace ASP_111.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IDateService _dateService;
+
+        public HomeController(ILogger<HomeController> logger, IDateService dateService)
+        {
+            _logger = logger;
+            _dateService = dateService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Intro()
+        {
+            return View();
+        }
+
+        public ViewResult Services()
+        {
+            ViewData["date"] = _dateService.GetDate();
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
